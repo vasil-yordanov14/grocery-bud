e32f6452d94be45dfd82de26dac2d6c2a47bbe54
vasil-yordanov14 <vasil_sh@abv.bg>
2022-07-20 20:20:29 +0300

    edit item

--- src/App.js
+++ src/App.js
@@ -20,6 +20,18 @@ function App() {
       showAlert(true, `danger`, `please enter value`);
     } else if (name && isEditing) {
       // edit
+      setList(
+        list.map((item) => {
+          if (item.id === editId) {
+            return { ...item, title: name };
+          }
+          return item;
+        })
+      );
+      setName(``);
+      setEditId(null);
+      setIsEditing(false);
+      showAlert(true, `success`, `value changed`);
     } else {
       showAlert(true, `success`, `item added to the list`);
       const newItem = { id: new Date().getTime().toString(), title: name };
@@ -39,6 +51,13 @@ function App() {
   const removeItem = (id) => {
     showAlert(true, `danger`, `item removed`);
     setList(list.filter((item) => item.id !== id));
+  };
+
+  const editItem = (id) => {
+    const specificItem = list.find((item) => item.id === id);
+    setIsEditing(true);
+    setEditId(id);
+    setName(specificItem.title);
   };
 
   return (
@@ -61,7 +80,7 @@ function App() {
       </form>
       {list.length > 0 && (
         <div className="grocery-container">
-          <List items={list} removeItem={removeItem} />
+          <List items={list} removeItem={removeItem} editItem={editItem} />
           <button className="clear-btn" onClick={clearList}>
             clear items
           </button>
